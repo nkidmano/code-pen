a9459b9ae4a1cc4a683b1a087967a760515e1eef
nkidmano <nobudy55@gmail.com>
2018-04-19 22:03:52 +0700

    added node-course

--- node-course/mongodb/index.js
+++ node-course/mongodb/index.js
@@ -26,14 +26,30 @@ async function createCourse() {
   console.log(result);
 }
 
+
+    // .find({ price: { $gt: 10, $lte: 20 } }) => greater than
+    // .find({ price: { $in: [10, 15, 20] } }) => query in array value
+
+    // .find() => logical query and or
+    // .or([ { author: 'Mosh' }, { isPublished: true } ])
+    // .and([ { author: 'Mosh' }])
+
+    // query string starts with Mosh
+    // .find({ author: /^Mosh/ })
+
+    // query string ends with Hamedani
+    // .find({ author: /Hamedani$/i }) // "i" mean no case sensitive
+
+    // query string contains Mosh
+    // .find({ author: /.*Mosh.*/ })
+
 async function getCourses() {
   const courses = await Course
     .find({ author: 'Mosh', isPublished: true })
-    // .find({ price: { $gt: 10, $lte: 20 } })
-    // .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 }) // sort the document in the ascended order
-    .select({ name: 1, tags: 1 });
+    .select({ name: 1, tags: 1 })
+    .count();
   console.log(courses);
 }
 
