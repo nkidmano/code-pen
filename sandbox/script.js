01cb9b7c167af960b834028e8c8384506343d012
nkidmano <nobudy55@gmail.com>
2018-06-21 19:55:49 +0700

    added es6 lecture to snippet

---
+++ sandbox/script.js
@@ -0,0 +1,14 @@
+for (let i = 1; i < 100; i++) {
+  let output = '';
+  if (i % 3 === 0) output += 'Fizz';
+  if (i % 5 === 0) output += 'Buzz';
+  console.log(output || i);
+}
+
+function FizzBuzz(repeat) {
+  for( let i = 1; i <= repeat; i++)
+  let output = '';
+  if (i % 3 === 0) output += 'Fizz';
+  if (i % 5 === 0) output += 'Buzz';
+  console.log(output || i);
+}
